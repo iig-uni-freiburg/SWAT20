cc17e9d47ecf49a069405bb36d63db948bbf7ed1
jholderer <holderer@iig.uni-freiburg.de>
2013-10-23 13:23:50 +0000

    Changed Popup-Menu to new editor type PopupMenu.java

--- src/de/unifreiburg/iig/bpworkbench2/editor/gui/PopupMenu.java
+++ src/de/unifreiburg/iig/bpworkbench2/editor/gui/PopupMenu.java
@@ -9,32 +9,32 @@ import de.unifreiburg.iig.bpworkbench2.editor.soul.Properties;
 
 public class PopupMenu extends JPopupMenu {
 
-    public PopupMenu(PNMLEditor pnmlEditor) {
-        boolean selected = !pnmlEditor.getGraphComponent().getGraph().isSelectionEmpty();
+    public PopupMenu(PTNEditor ptnEditor) {
+        boolean selected = !ptnEditor.getGraphComponent().getGraph().isSelectionEmpty();
 
-        add(pnmlEditor.bind("Undo", new HistoryAction(true),
+        add(ptnEditor.bind("Undo", new HistoryAction(true),
                 "/images/undo.gif"));
 
         addSeparator();
-        add(pnmlEditor.bind("Cut", TransferHandler.getCutAction(),
+        add(ptnEditor.bind("Cut", TransferHandler.getCutAction(),
                 "/images/cut.gif")).setEnabled(selected);
-        add(pnmlEditor.bind("Copy", TransferHandler.getCopyAction(),
+        add(ptnEditor.bind("Copy", TransferHandler.getCopyAction(),
                 "/images/copy.gif")).setEnabled(selected);
-        add(pnmlEditor.bind("Paste", TransferHandler.getPasteAction(),
+        add(ptnEditor.bind("Paste", TransferHandler.getPasteAction(),
                 "/images/paste.gif"));
 
         addSeparator();
-        add(pnmlEditor.bind("Delete", mxGraphActions.getDeleteAction(),
+        add(ptnEditor.bind("Delete", mxGraphActions.getDeleteAction(),
                 "/images/delete.gif")).setEnabled(selected);
 
         addSeparator();
 
         JMenu menu = (JMenu) add(new JMenu("Format"));
-        MenuBar.populateFormatMenu(menu, pnmlEditor);
-        add(pnmlEditor.bind("Edit", mxGraphActions.getEditAction())).setEnabled(selected);
+        MenuBar.populateFormatMenu(menu, ptnEditor);
+        add(ptnEditor.bind("Edit", mxGraphActions.getEditAction())).setEnabled(selected);
 
         addSeparator();
-        add(pnmlEditor.bind("selectVertices", mxGraphActions.getSelectVerticesAction()));
-        add(pnmlEditor.bind("selectEdges", mxGraphActions.getSelectEdgesAction()));
+        add(ptnEditor.bind("selectVertices", mxGraphActions.getSelectVerticesAction()));
+        add(ptnEditor.bind("selectEdges", mxGraphActions.getSelectEdgesAction()));
     }
 }
