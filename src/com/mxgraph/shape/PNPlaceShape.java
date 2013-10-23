0444678fc74df46814439f55c7ab613f0b934ee0
jholderer <holderer@iig.uni-freiburg.de>
2013-10-23 13:27:04 +0000

    Changed PNPlaceShape.java and PNTransitionShape.java because some of their methods are now done with Graph.java

--- src/com/mxgraph/shape/PNPlaceShape.java
+++ src/com/mxgraph/shape/PNPlaceShape.java
@@ -6,12 +6,14 @@ import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Shape;
-import java.awt.geom.Area;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Ellipse2D.Float;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
+import java.util.Map.Entry;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import com.mxgraph.canvas.mxGraphics2DCanvas;
 import com.mxgraph.model.mxCell;
@@ -23,15 +25,14 @@ import de.invation.code.toval.graphic.CircularPointGroup;
 import de.invation.code.toval.graphic.GraphicUtils;
 import de.invation.code.toval.graphic.PColor;
 import de.invation.code.toval.graphic.Position;
-import de.invation.code.toval.graphic.VisualCircularPointGroup;
-import de.invation.code.toval.types.Multiset;
 import de.invation.code.toval.validate.ParameterException;
 import de.uni.freiburg.iig.telematik.sepia.graphic.AbstractGraphicalPN;
-import de.uni.freiburg.iig.telematik.sepia.graphic.CPNGraphics;
-import de.uni.freiburg.iig.telematik.sepia.graphic.TokenGraphics;
-import de.uni.freiburg.iig.telematik.sepia.petrinet.cpn.CPNFlowRelation;
-import de.uni.freiburg.iig.telematik.sepia.petrinet.cpn.CPNPlace;
-import de.uni.freiburg.iig.telematik.sepia.petrinet.cpn.abstr.AbstractCPNPlace;
+import de.uni.freiburg.iig.telematik.sepia.graphic.netgraphics.CPNGraphics;
+import de.uni.freiburg.iig.telematik.sepia.graphic.netgraphics.NodeGraphics;
+import de.uni.freiburg.iig.telematik.sepia.petrinet.AbstractPNNode;
+import de.uni.freiburg.iig.telematik.sepia.petrinet.AbstractPlace;
+import de.uni.freiburg.iig.telematik.sepia.petrinet.AbstractTransition;
+import de.uni.freiburg.iig.telematik.sepia.petrinet.pt.PTPlace;
 
 public class PNPlaceShape extends mxBasicShape
 {
@@ -47,6 +48,7 @@ public class PNPlaceShape extends mxBasicShape
 	protected Point center; 
 	protected CircularPointGroup pointGroup;
 	CircularPointGroup circularPointGroup;
+	
 
 	private int k;
 	
@@ -58,32 +60,82 @@ public class PNPlaceShape extends mxBasicShape
 		
 		
 Object value = ((mxCell)state.getCell()).getValue();
-		if((value == null) && (((mxCell)state.getCell()).getParent() !=null)){
-			System.out.println(((mxCell)state.getCell()).getParent().getValue());
-//		System.out.println(((mxCell)state.getCell()).getParent().getValue());
-		AbstractGraphicalPN<?, ?, ?, ?, ?> n = (AbstractGraphicalPN<?, ?, ?, ?, ?>) ((mxCell)state.getCell()).getParent().getValue();
-		try {
-			n.getPetriNet().addPlace(((mxCell)state.getCell()).getId(), "name");
-		} catch (ParameterException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		 ((mxCell)state.getCell()).setValue(n);
-		 mxCell cell = ((mxCell)state.getCell());
-//		 cell.setId("new3");
-		 state.setLabel("");
-//		 state.setCell(cell);
-//		cell.notifyAll();
-
-//		 
-		}
+//		if((value == null) && (((mxCell)state.getCell()).getParent() !=null)){
+//		AbstractGraphicalPN<?, ?, ?, ?, ?> n = (AbstractGraphicalPN<?, ?, ?, ?, ?>) ((mxCell)state.getCell()).getParent().getValue();
+//		SortedMap<Integer, String> placeSortedMap = new TreeMap<Integer, String>();
+//		
+//		Collection<?> col = n.getPetriNet().getPlaces();
+//		for(Object o:col){
+//			if(o instanceof PTPlace)
+//			{
+//				PTPlace place = (PTPlace)o;
+//
+//				addPlaceToMap(placeSortedMap, place.getName(), "p");
+//			}
+//		
+//		
+//		}	
+//		try {
+//			n.getPetriNet().addPlace("p"+ getLowestIndex(placeSortedMap));
+//		} catch (ParameterException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}	
+//		int index = getLowestIndex(placeSortedMap);
+//		addPlaceToMap(placeSortedMap, "p"+ index, "p");
+//		
+//		
+////		try {
+////			n.getPetriNet().addPlace(((mxCell)state.getCell()).getId(), "name");
+////		} catch (ParameterException e) {
+////			// TODO Auto-generated catch block
+////			e.printStackTrace();
+////		}
+//		 ((mxCell)state.getCell()).setValue("p" + index);
+//		 ((mxCell)state.getCell()).setId("p" + index);
+//		 mxCell cell = ((mxCell)state.getCell());
+//		  mxGeometry geom = new mxGeometry(0, 0, cell.getGeometry().getWidth(), 10);
+//		  geom.setOffset(new mxPoint(0, cell.getGeometry().getHeight() + 5));
+//          geom.setRelative(true);
+//			mxCell label = new mxCell(n.getPetriNet().getPlace(cell.getId()).getLabel(), geom, "shape=none;fontSize=12");
+//        
+//        label.setVertex(true);
+//        label.setConnectable(false);
+//        cell.insert(label);
+////     cell.notify();
+////		 cell.setId("new3");
+//		 state.setLabel("");
+////		 state.setCell(cell);
+////		cell.notifyAll();
+//
+////		 
+//		}
 		if(value instanceof AbstractGraphicalPN<?, ?, ?, ?, ?>){
 			AbstractGraphicalPN<?, ?, ?, ?, ?> n = (AbstractGraphicalPN<?, ?, ?, ?, ?>)  value;
 			Object place = n.getPetriNet().getPlace(((mxCell)state.getCell()).getId());
 //			Counts all tokens
 //			Iterator<Integer> countIt = tokenCount.iterator();
-			k = ((AbstractCPNPlace<CPNFlowRelation>) place).getTokenCount();
-			System.out.println(k);
+//			k = ((AbstractCPNPlace<CPNFlowRelation>) place).getTokenCount();
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			k=1;
 //			while(countIt.hasNext()){
 //				k += countIt.next();}
 
@@ -117,30 +169,26 @@ Object value = ((mxCell)state.getCell()).getValue();
 				colors = g.getColors();
 			}
 			//			if();
-			Set<String> keyset = ((AbstractCPNPlace<CPNFlowRelation>) place).getState().support();
-
-			for (String s : keyset){
-//				System.out.println(s);
-				try {
-//					System.out.println(((AbstractCPNPlace<CPNFlowRelation>) place).getState().multiplicity(s));
-					
-					Color color = colors.get(s);
-//					System.out.println(color + "-" + s);
-					int number = ((AbstractCPNPlace<CPNFlowRelation>) place).getState().multiplicity(s);
-					PColor pco;
-					if(color !=null)
-					pco = new PColor(color.getRed(), color.getGreen(), color.getBlue());
-					else {pco = PColor.black;}
-					
-					circularPointGroup.addPoints(pco, number);
-				} catch (ParameterException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+//			Set<String> keyset = ((AbstractCPNPlace<CPNFlowRelation>) place).getState().support();
+//
+//			for (String s : keyset){
+//				try {
+//					
+//					Color color = colors.get(s);
+//					int number = ((AbstractCPNPlace<CPNFlowRelation>) place).getState().multiplicity(s);
+//					PColor pco;
+//					if(color !=null)
+//					pco = new PColor(color.getRed(), color.getGreen(), color.getBlue());
+//					else {pco = PColor.black;}
+//					
+//					circularPointGroup.addPoints(pco, number);
+//				} catch (ParameterException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//			}
 		
 //for(TokenGraphics tg : n.getPetriNetGraphics().getTokenGraphics().get(place)){
-//	System.out.println(tg.getColorName().);
 //}
 				
 	
@@ -149,10 +197,11 @@ Object value = ((mxCell)state.getCell()).getValue();
 		
 		this.dimension = dimension;
 		if(k>0){
-		this.dimension = new Dimension(circularPointGroup.getRequiredDiameter(), circularPointGroup.getRequiredDiameter());
-		center = new Point(temp.x + temp.width/2,temp.y + temp.height/2);
-		this.pointGroup = circularPointGroup;
-		Graphics g = drawPoints(canvas, temp);}
+//		this.dimension = new Dimension(circularPointGroup.getRequiredDiameter(), circularPointGroup.getRequiredDiameter());
+//		center = new Point(temp.x + temp.width/2,temp.y + temp.height/2);
+//		this.pointGroup = circularPointGroup;
+//		Graphics g = drawPoints(canvas, temp);}
+		}
 		Float shape = new Ellipse2D.Float(temp.x, temp.y, temp.width, temp.height);
 
 
@@ -177,7 +226,7 @@ Object value = ((mxCell)state.getCell()).getValue();
 	public void paintShape(mxGraphics2DCanvas canvas, mxCellState state)
 	{
 		Shape shape = createShape(canvas, state);
-		
+
 		
 		if (shape != null)
 		{
@@ -217,5 +266,58 @@ Object value = ((mxCell)state.getCell()).getValue();
 //				}
 			}
 		}
+	}
+	/**
+	 * gets the NodeGraphicAttribute of the given Node. 
+	 * 
+	 * @param graphicalNet
+	 * @param node
+	 * @return nodeGraphics 
+	 */
+	private static NodeGraphics getNodeGraphics(
+			AbstractGraphicalPN<?, ?, ?, ?, ?> graphicalNet,
+			AbstractPNNode<?> node) {
+		NodeGraphics nodeGraphics = null;
+		Map<?, NodeGraphics> graphics = null;
+
+		if (node instanceof AbstractPlace)
+			graphics = graphicalNet.getPetriNetGraphics().getPlaceGraphics();
+		if (node instanceof AbstractTransition)
+			graphics = graphicalNet.getPetriNetGraphics()
+					.getTransitionGraphics();
+
+		for (Entry<?, ?> place : graphics.entrySet()) {
+			if (place.getKey() == node)
+				nodeGraphics = (NodeGraphics) place.getValue();
+		}
+		return nodeGraphics;
+	}
+	private void addPlaceToMap(Map<Integer, String> a, String string, String nameConvention) {
+		if(string.startsWith(nameConvention) && isInteger(string.substring(nameConvention.length()))){
+			Integer integer = new Integer(string.substring(nameConvention.length()));
+			
+
+			
+			a.put(integer,nameConvention + integer);
+		}
+		}
+	
+	public boolean isInteger(String string) {
+	    try {
+	        Integer.valueOf(string);
+	        return true;
+	    } catch (NumberFormatException e) {
+	        return false;
+	    }
+	}
+	public int getLowestIndex(SortedMap<Integer, String> a) {
+		if(!a.isEmpty()){
+		for(int i = 1;i<=a.lastKey();i++){
+			if(a.get(i) == null)
+			return i;
+		}
+		;
+		return a.lastKey()+1;}
+		else{return 1;}
 	}
 }
--- src/com/mxgraph/shape/PNTransitionShape.java
+++ src/com/mxgraph/shape/PNTransitionShape.java
@@ -1,13 +1,21 @@
 package com.mxgraph.shape;
 
 import java.awt.Rectangle;
+import java.util.Collection;
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import com.mxgraph.canvas.mxGraphics2DCanvas;
+import com.mxgraph.model.mxCell;
 import com.mxgraph.swing.util.mxSwingConstants;
 import com.mxgraph.util.mxConstants;
 import com.mxgraph.util.mxUtils;
 import com.mxgraph.view.mxCellState;
+
+import de.invation.code.toval.validate.ParameterException;
+import de.uni.freiburg.iig.telematik.sepia.graphic.AbstractGraphicalPN;
+import de.uni.freiburg.iig.telematik.sepia.petrinet.pt.PTTransition;
 
 public class PNTransitionShape extends mxBasicShape
 {
@@ -18,7 +26,77 @@ public class PNTransitionShape extends mxBasicShape
 	public void paintShape(mxGraphics2DCanvas canvas, mxCellState state)
 	{
 		Map<String, Object> style = state.getStyle();
-state.setLabel("high");
+state.setLabel("");
+
+
+////state.getCell()
+//mxCell cell = (mxCell) state.getCell();
+//Object value = ((mxCell)state.getCell()).getValue();
+//
+//
+//if((value == null) && (((mxCell)state.getCell()).getParent() !=null)){
+
+//AbstractGraphicalPN<?, ?, ?, ?, ?> n = (AbstractGraphicalPN<?, ?, ?, ?, ?>) ((mxCell)state.getCell()).getParent().getValue();
+//SortedMap<Integer, String> transitionSortedMap = new TreeMap<Integer, String>();
+//
+//Collection<?> col = n.getPetriNet().getTransitions();
+//for(Object o:col){
+
+//	if(o instanceof PTTransition)
+//	{
+//		PTTransition transition = (PTTransition)o;
+//
+//		addTransitionToMap(transitionSortedMap, transition.getName(), "t");
+//	}
+//
+//
+//}	
+//try {
+//	n.getPetriNet().addTransition("t"+ getLowestIndex(transitionSortedMap));
+//} catch (ParameterException e) {
+//	// TODO Auto-generated catch block
+//	e.printStackTrace();
+//}	
+//int index = getLowestIndex(transitionSortedMap);
+//addTransitionToMap(transitionSortedMap, "t"+ index, "t");
+//
+//
+////try {
+////	n.getPetriNet().addTransition(((mxCell)state.getCell()).getId(), "name");
+////} catch (ParameterException e) {
+////	// TODO Auto-generated catch block
+////	e.printStackTrace();
+////}
+// ((mxCell)state.getCell()).setValue("t" + index);
+// ((mxCell)state.getCell()).setId("t" + index);
+// cell.setValue("t" + index);
+// cell.setId("t" + index);
+//// mxCell cell = ((mxCell)state.getCell());
+//// cell.setId("new3");
+// state.setLabel("");
+//// state.setCell(cell);
+////cell.notifyAll();
+//
+//// 
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		if (mxUtils.isTrue(style, mxConstants.STYLE_ROUNDED, false))
 		{
 			Rectangle tmp = state.getRectangle();
@@ -112,6 +190,35 @@ state.setLabel("high");
 			}
 		}
 		return arcSize;
+	}
+	
+	private void addTransitionToMap(Map<Integer, String> a, String string, String nameConvention) {
+		if(string.startsWith(nameConvention) && isInteger(string.substring(nameConvention.length()))){
+			Integer integer = new Integer(string.substring(nameConvention.length()));
+			
+
+			
+			a.put(integer,nameConvention + integer);
+		}
+		}
+	
+	public boolean isInteger(String string) {
+	    try {
+	        Integer.valueOf(string);
+	        return true;
+	    } catch (NumberFormatException e) {
+	        return false;
+	    }
+	}
+	public int getLowestIndex(SortedMap<Integer, String> a) {
+		if(!a.isEmpty()){
+		for(int i = 1;i<=a.lastKey();i++){
+			if(a.get(i) == null)
+			return i;
+		}
+		;
+		return a.lastKey()+1;}
+		else{return 1;}
 	}
 
 }
