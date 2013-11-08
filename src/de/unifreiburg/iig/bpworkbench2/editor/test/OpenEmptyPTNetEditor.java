b7e6ea7c8e556e2d69dc094f36cf47c8cbd4b776
jholderer <holderer@iig.uni-freiburg.de>
2013-11-08 12:24:17 +0000

    Added openSampleNet Method in OpenEmptyPTNetEditor.java

--- src/de/unifreiburg/iig/bpworkbench2/editor/test/OpenEmptyPTNetEditor.java
+++ src/de/unifreiburg/iig/bpworkbench2/editor/test/OpenEmptyPTNetEditor.java
@@ -10,18 +10,22 @@ import javax.swing.JPanel;
 import de.invation.code.toval.graphic.DisplayFrame;
 import de.invation.code.toval.parser.ParserException;
 import de.invation.code.toval.validate.ParameterException;
+import de.uni.freiburg.iig.telematik.sepia.graphic.AbstractGraphicalPN;
+import de.uni.freiburg.iig.telematik.sepia.graphic.GraphicalPTNet;
+import de.uni.freiburg.iig.telematik.sepia.parser.pnml.PNMLParser;
+import de.uni.freiburg.iig.telematik.sepia.petrinet.pt.PTFlowRelation;
 import de.unifreiburg.iig.bpworkbench2.editor.PNEditor;
 import de.unifreiburg.iig.bpworkbench2.editor.PTNetEditor;
 
 public class OpenEmptyPTNetEditor {
 	
-	public final static String PNML = PNEditor.class.getResource(
-			"/samples/samplePTnet.pnml").getPath();
+	public final static String PNML = PNEditor.class.getResource("/samples/samplePTnet.pnml").getPath();
 	public final static String LABELING = PNEditor.class.getResource(
 			"/samples/sampleIFnetLabeling01.xml").getPath();
 
 	public static void main(String[] args) throws IOException, ParserException, ParameterException {
-		testEmptyNet();
+//		testEmptyNet();
+		openSampleNet();
 	}
 	
 	private static void testEmptyNet() throws ParameterException{
@@ -45,75 +49,38 @@ public class OpenEmptyPTNetEditor {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
-//	private static void doSomethingFancy(){
-//		// Create new JFrame
-//				JFrame frame = new JFrame();
-//				JPanel panel = null;
-//				frame.setTitle("PTNet Editor");
-//				frame.setSize(450, 300);
-//
-//				/*
-//				 * LOAD PNML-EDITOR parser partially finished, you may create an empty
-//				 * editor with "null", or an Editor to Load a PT-Net oder CPN by
-//				 * inserting its File-Object
-//				 */
-//				// panel = new PNMLEditor(null);
-//				String filePath = PNEditor.class.getResource("/samples/samplePTnet.pnml").getPath();
-//				
-////				/*
-////				 * PetriNet
-////				 */
-//				AbstractGraphicalPN<?, ?, ?, ?, ?, ?, ?> netContainer = new PNMLParser().parse(PNML, true, false);
-////				AbstractPetriNet<?, ?, ?, ?, ?> petriNet = netContainer.getPetriNet();
-//				AbstractPetriNet<?, ?, ?, ?, ?> petriNet = new PTNet();
-////				ptNet.addPlace("place1");
-////				ptNet.addPlace("place2");
-////				ptNet.addTransition("trans1");
-////				ptNet.addFlowRelationPT("place1", "trans1");
-////				ptNet.addFlowRelationTP("trans1", "place2");
-////				try {
-////					ptNet.checkSoundness();
-////				} catch (PNValidationException e) {
-////					// TODO Auto-generated catch block
-////					e.printStackTrace();
-////				} catch (PNSoundnessException e) {
-////					// TODO Auto-generated catch block
-////					e.printStackTrace();
-////				}
-////				PTGraphics ptNetGraphics = new PTGraphics();
-////				ptNetGraphics.setPlaceGraphics(placeGraphics);
-//				
-////				GraphicalPTNet netContainer = new GraphicalPTNet(new PTNet(), new PTGraphics());
-//				
-//				String userHome = System.getProperty("user.home");
-//				File file = new File(userHome);
-//				//distinguish between different net-types
-//				if (petriNet instanceof PTNet) {
-//					panel = new PTNetEditor(file, (GraphicalPTNet) petriNet);
-//				}
-//
-//				if (petriNet instanceof CPN) {
-//					panel = new CPNEditor(netContainer);
-//				}
-//
-//				if (petriNet instanceof CWN) {
-//					panel = new CWNEditor(netContainer);
-//
-//				}
-//
-//				if (petriNet instanceof IFNet) {
-//					panel = new IFNetEditor(netContainer);
-//				}
-//				
-//				
-//				// set background black
-//				panel.setBackground(Color.black);
-//
-//				// add Editor to JFrame
-//				frame.add(panel);
-//
-//				// show all
-//				frame.setVisible(true);
-//				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//	}
+	
+	private static void openSampleNet() throws IOException, ParserException,
+			ParameterException {
+		// Create new JFrame
+		JFrame frame = new JFrame();
+		AbstractGraphicalPN<?, ?, ?, ?, ?, ?, ?> netContainer = new PNMLParser()
+				.parse(PNML);
+
+		GraphicalPTNet graphicalPTNet = (GraphicalPTNet) netContainer;
+		for (Object o : graphicalPTNet.getPetriNet().getFlowRelations()) {
+			PTFlowRelation fr = ((PTFlowRelation) o);
+
+			System.out.println("NAME:" + fr.getName() + "\nWEIGHT:"
+					+ fr.getWeight());
+			System.out.println(graphicalPTNet.getPetriNet()
+					.getFlowRelation(fr.getName()).getWeight());
+		}
+
+		JPanel panel = new PTNetEditor(((GraphicalPTNet) netContainer),
+				new File(PNML));
+		frame.setTitle("PTNet Editor");
+		frame.setSize(450, 300);
+
+		// set background black
+		panel.setBackground(Color.black);
+
+		// add Editor to JFrame
+		frame.add(panel);
+
+		// show all
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
 }
