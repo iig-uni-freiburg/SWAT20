e7d87a239c30db123963d2e3d946b4e6c9cdd0f5
jholderer <holderer@iig.uni-freiburg.de>
2013-11-08 00:01:55 +0000

    Updated emptyNet test

--- src/de/unifreiburg/iig/bpworkbench2/editor/test/OpenEmptyPTNetEditor.java
+++ src/de/unifreiburg/iig/bpworkbench2/editor/test/OpenEmptyPTNetEditor.java
@@ -1,7 +1,11 @@
 package de.unifreiburg.iig.bpworkbench2.editor.test;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import de.invation.code.toval.graphic.DisplayFrame;
 import de.invation.code.toval.parser.ParserException;
@@ -23,7 +27,22 @@ public class OpenEmptyPTNetEditor {
 	private static void testEmptyNet() throws ParameterException{
 		String userHome = System.getProperty("user.home");
 		File file = new File(userHome + "test");
-		new DisplayFrame(new PTNetEditor(file), true);
+//		new DisplayFrame(new PTNetEditor(file), true);
+		JFrame frame = new JFrame();
+		JPanel panel = new PTNetEditor(file);
+		frame.add(panel);
+		frame.setTitle("PTNet Editor");
+		frame.setSize(450, 300);
+		
+		// set background black
+		panel.setBackground(Color.black);
+
+		// add Editor to JFrame
+		frame.add(panel);
+
+		// show all
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 //	private static void doSomethingFancy(){
