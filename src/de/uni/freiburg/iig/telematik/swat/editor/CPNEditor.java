8007203ffa72d09805977e86d9a7abc329d37182
jholderer <holderer@iig.uni-freiburg.de>
2013-11-15 09:37:31 +0000

    new folder strucutre

---
+++ src/de/uni/freiburg/iig/telematik/swat/editor/CPNEditor.java
@@ -0,0 +1,68 @@
+package de.uni.freiburg.iig.telematik.swat.editor;
+
+import java.io.File;
+
+import de.invation.code.toval.validate.ParameterException;
+import de.uni.freiburg.iig.telematik.sepia.graphic.GraphicalCPN;
+import de.uni.freiburg.iig.telematik.sepia.graphic.netgraphics.CPNGraphics;
+import de.uni.freiburg.iig.telematik.sepia.petrinet.AbstractFlowRelation;
+import de.uni.freiburg.iig.telematik.sepia.petrinet.cpn.CPN;
+import de.uni.freiburg.iig.telematik.swat.editor.graph.PNGraphComponent;
+import de.uni.freiburg.iig.telematik.swat.editor.properties.PNProperties;
+import de.unifreiburg.iig.bpworkbench2.editor.EditorPopupMenu;
+
+public class CPNEditor extends AbstractCPNEditor {
+
+	private static final long serialVersionUID = 7463202384539027183L;
+
+	public CPNEditor(File fileReference) throws ParameterException {
+		super(fileReference);
+	}
+
+	public CPNEditor(GraphicalCPN netContainer, File fileReference) throws ParameterException {
+		super(netContainer, fileReference);
+	}
+
+	@Override
+	public GraphicalCPN getNetContainer() {
+		return (GraphicalCPN) super.getNetContainer();
+	}
+	
+	@Override
+	public GraphicalCPN createNetContainer() {
+		return new GraphicalCPN(new CPN(), new CPNGraphics());
+	}
+
+	@Override
+	protected PNProperties createPNProperties() {
+		//TODO:		return new CPNProperties(getNetContainer());
+		return null;
+	}
+
+	@SuppressWarnings("rawtypes") 
+	@Override
+	protected String getArcConstraint(AbstractFlowRelation relation) {
+		// TODO: Do something
+		return null;
+	}
+
+	@Override
+	protected PNProperties getPNProperties() {
+		//TODO:		return (CPNProperties) super.getPNProperties();
+		return null;
+	}
+
+	@Override
+	protected PNGraphComponent createGraphComponent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EditorPopupMenu getPopupMenu() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
---
+++ src/de/uni/freiburg/iig/telematik/swat/editor/IFNetEditor.java
@@ -0,0 +1,68 @@
+package de.uni.freiburg.iig.telematik.swat.editor;
+
+import java.io.File;
+
+import de.invation.code.toval.validate.ParameterException;
+import de.uni.freiburg.iig.telematik.sepia.graphic.GraphicalIFNet;
+import de.uni.freiburg.iig.telematik.sepia.graphic.netgraphics.IFNetGraphics;
+import de.uni.freiburg.iig.telematik.sepia.petrinet.AbstractFlowRelation;
+import de.uni.freiburg.iig.telematik.sepia.petrinet.ifnet.IFNet;
+import de.uni.freiburg.iig.telematik.swat.editor.graph.PNGraphComponent;
+import de.uni.freiburg.iig.telematik.swat.editor.properties.PNProperties;
+import de.unifreiburg.iig.bpworkbench2.editor.EditorPopupMenu;
+
+public class IFNetEditor extends AbstractCPNEditor {
+
+	private static final long serialVersionUID = 8612413737377657095L;
+
+	public IFNetEditor(File fileReference) throws ParameterException {
+		super(fileReference);
+	}
+
+	public IFNetEditor(GraphicalIFNet netContainer, File fileReference) throws ParameterException {
+		super(netContainer, fileReference);
+	}
+	
+	@Override
+	protected GraphicalIFNet createNetContainer() {
+		return new GraphicalIFNet(new IFNet(), new IFNetGraphics());
+	}
+
+	@Override
+	public GraphicalIFNet getNetContainer() {
+		return (GraphicalIFNet) super.getNetContainer();
+	}
+
+	@Override
+	protected PNProperties createPNProperties() {
+		//TODO:		return new IFNetProperties(getNetContainer());
+		return null;
+	}
+
+	@SuppressWarnings("rawtypes") 
+	@Override
+	protected String getArcConstraint(AbstractFlowRelation relation) {
+		// TODO: Do something
+		return null;
+	}
+
+	@Override
+	protected PNProperties getPNProperties() {
+		//TODO:		return (IFNetProperties) super.getPNProperties();
+		return null;
+	}
+
+	@Override
+	protected PNGraphComponent createGraphComponent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EditorPopupMenu getPopupMenu() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
