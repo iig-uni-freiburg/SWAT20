d755c211aee4452e16e53d81613c8d5b05f95acf
jholderer <holderer@iig.uni-freiburg.de>
2013-11-15 08:40:40 +0000

    Added openSampleNetWithProperties method to test properties in OpenEmptyPTNetEditor.java

--- src/de/unifreiburg/iig/bpworkbench2/editor/test/OpenEmptyPTNetEditor.java
+++ src/de/unifreiburg/iig/bpworkbench2/editor/test/OpenEmptyPTNetEditor.java
@@ -1,5 +1,6 @@
 package de.unifreiburg.iig.bpworkbench2.editor.test;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
@@ -13,6 +14,7 @@ import de.uni.freiburg.iig.telematik.sepia.graphic.AbstractGraphicalPN;
 import de.uni.freiburg.iig.telematik.sepia.graphic.GraphicalPTNet;
 import de.uni.freiburg.iig.telematik.sepia.parser.pnml.PNMLParser;
 import de.uni.freiburg.iig.telematik.sepia.petrinet.pt.PTFlowRelation;
+import de.uni.freiburg.iig.telematik.swat.editor.properties.PropertiesView;
 import de.unifreiburg.iig.bpworkbench2.editor.PNEditor;
 import de.unifreiburg.iig.bpworkbench2.editor.PTNetEditor;
 
@@ -24,7 +26,8 @@ public class OpenEmptyPTNetEditor {
 
 	public static void main(String[] args) throws IOException, ParserException, ParameterException {
 //		testEmptyNet();
-		openSampleNet();
+//		openSampleNet();
+		openSampleNetWithProperties();
 	}
 	
 	private static void testEmptyNet() throws ParameterException{
@@ -76,4 +79,43 @@ public class OpenEmptyPTNetEditor {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
+	
+	private static void openSampleNetWithProperties() throws IOException, ParserException, ParameterException {
+		// Create new JFrame
+		JFrame frame = new JFrame();
+		AbstractGraphicalPN<?, ?, ?, ?, ?, ?, ?> netContainer = new PNMLParser().parse(PNML, false,false);
+
+
+		// GraphicalPTNet graphicalPTNet = (GraphicalPTNet) netContainer;
+		// for (PTFlowRelation relation : graphicalPTNet.getPetriNet().getFlowRelations()) {
+		// System.out.println("NAME:" + relation.getName() + "\nWEIGHT:" + relation.getWeight());
+		// System.out.println(graphicalPTNet.getPetriNet().getFlowRelation(relation.getName()));
+		//// System.out.println(graphicalPTNet.getPetriNet().getFlowRelation(relation.getName()).getWeight());
+		// }
+
+
+		JPanel panel = new PTNetEditor(((GraphicalPTNet) netContainer), new File(PNML));
+		PropertiesView pV = ((PNEditor) panel).getPropertiesView();
+		frame.setTitle("PTNet Editor");
+		frame.setSize(450, 300);
+
+
+		// set background black
+		panel.setBackground(Color.black);
+		frame.setLayout(new BorderLayout());
+		// add Editor to JFrame
+		frame.getContentPane().add(panel, BorderLayout.LINE_START);
+		frame.getContentPane().add(pV, BorderLayout.LINE_END);
+		// frame.add(pV);
+		// frame.add(panel);
+
+
+		// show all
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+		}
+	
+	
 }
