24045b4d784b2e73a555d5e054220fa3212fae20
rzahoransky <r.zahoransky@gmx.de>
2014-07-16 15:10:51 +0000

    readfile implementation replaced

--- src/de/uni/freiburg/iig/telematik/swat/lukas/IOUtils.java
+++ src/de/uni/freiburg/iig/telematik/swat/lukas/IOUtils.java
@@ -1,11 +1,14 @@
 package de.uni.freiburg.iig.telematik.swat.lukas;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+//import java.nio.charset.Charset;
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
 
 public class IOUtils {
 	
@@ -52,7 +55,7 @@ public class IOUtils {
     	}
 	}
 
-	public static String readFile(String filepath, Charset encoding) {
+	/*public static String readFile(String filepath, Charset encoding) {
 		
 		String str = "";
 		try {
@@ -64,5 +67,29 @@ public class IOUtils {
 		}
 		return str;
 		  
+	}*/
+	
+	public static String readFile(String filepath) {
+		
+		BufferedReader bufferedReader;
+		try {
+			bufferedReader = new BufferedReader(new FileReader(filepath));
+			StringBuffer stringBuffer = new StringBuffer();
+			String line = null;
+			
+			while((line = bufferedReader.readLine())!=null){
+				stringBuffer.append(line).append("\n");
+			}
+			bufferedReader.close();
+			return stringBuffer.toString();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
