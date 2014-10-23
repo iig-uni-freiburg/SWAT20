e7d2707789238211aeb9d63c3b85095acc66ce36
adlange <lange@iig.uni-freiburg.de>
2014-10-23 11:47:26 +0000

    pattern factory code

---
+++ src/de/uni/freiburg/iig/telematik/swat/lukas/patterns/factory/ParamValue.java
@@ -0,0 +1,38 @@
+package de.uni.freiburg.iig.telematik.swat.lukas.patterns.factory;
+
+public class ParamValue {
+	
+	public void setOperandName(String mOperandName) {
+		this.mOperandName = mOperandName;
+	}
+
+	private String mOperandName;
+	private OperandType mOperandType;
+
+	public ParamValue(String operandStr, OperandType type) {
+		mOperandName = operandStr;
+		mOperandType = type;
+	}
+	
+	public String getOperandName() {
+		return mOperandName;
+	}
+	
+	public OperandType getOperandType() {
+		return mOperandType;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if(!(o instanceof ParamValue)) {
+			return false;
+		}
+		ParamValue v=(ParamValue) o;
+		//System.out.println("me: "+mOperandName);
+		//System.out.println("he: "+v.getOperandName());
+		return mOperandName.equals(v.getOperandName()) && mOperandType == v.getOperandType();
+	}
+	public String toString() {
+		return mOperandName;
+	}
+}
---
+++ src/de/uni/freiburg/iig/telematik/swat/lukas/patterns/factory/UnsupportedPattern.java
@@ -0,0 +1,11 @@
+package de.uni.freiburg.iig.telematik.swat.lukas.patterns.factory;
+
+public class UnsupportedPattern extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public UnsupportedPattern(String msg) {
+		super(msg);
+	}
+
+}
