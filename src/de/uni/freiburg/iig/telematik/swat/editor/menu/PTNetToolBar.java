6e5475be093a42d64c5dddc9bb2420080fd5a664
jholderer <holderer@iig.uni-freiburg.de>
2014-12-10 15:08:37 +0000

    new toolbar Structure

---
+++ src/de/uni/freiburg/iig/telematik/swat/editor/menu/PTNetToolBar.java
@@ -0,0 +1,19 @@
+package de.uni.freiburg.iig.telematik.swat.editor.menu;
+
+import de.uni.freiburg.iig.telematik.swat.editor.PNEditor;
+import de.uni.freiburg.iig.telematik.swat.editor.exception.EditorToolbarException;
+
+public class PTNetToolBar extends AbstractToolBar {
+
+	public PTNetToolBar(final PNEditor pnEditor, int orientation) throws EditorToolbarException {
+		super(pnEditor, orientation);
+	}
+
+	@Override
+	protected void addNetSpecificToolbar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
