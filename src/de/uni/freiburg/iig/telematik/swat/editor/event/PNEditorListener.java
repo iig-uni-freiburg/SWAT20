e601fd2c7c838bda3a72400a4e411ac6250bc7d2
GerdHolz <phanthomas@invation.de>
2014-12-11 11:28:42 +0000

    Moving Wolfgang out of SWAT...

--- src/de/uni/freiburg/iig/telematik/swat/editor/event/PNEditorListener.java
+++
@@ -1,7 +0,0 @@
-package de.uni.freiburg.iig.telematik.swat.editor.event;
-
-public interface PNEditorListener {
-	
-	public void modificationStateChanged(boolean modified);
-
-}
--- src/de/uni/freiburg/iig/telematik/swat/editor/properties/tree/PNTreeNodeType.java
+++
@@ -1,5 +0,0 @@
-package de.uni.freiburg.iig.telematik.swat.editor.properties.tree;
-
-public enum PNTreeNodeType {
-	PLACES, TRANSITIONS, ARCS, PLACE,TRANSITION,ARC,ROOT,LEAF, 
-}
