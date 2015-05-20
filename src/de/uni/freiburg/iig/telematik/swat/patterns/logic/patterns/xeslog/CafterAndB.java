0c9d2ed6a64c5b2a2eae5ea9dfaf6b640b0d5217
rzahoransky <r.zahoransky@gmx.de>
2015-05-20 11:49:33 +0000

    new SCIFF rule: C after A and B Rule update: B or C after A

--- src/de/uni/freiburg/iig/telematik/swat/patterns/logic/patterns/xeslog/CafterAndB.java
+++ src/de/uni/freiburg/iig/telematik/swat/patterns/logic/patterns/xeslog/CafterAndB.java
@@ -26,9 +26,9 @@ import de.uni.freiburg.iig.telematik.swat.patterns.logic.patterns.PatternRequire
 import de.uni.freiburg.iig.telematik.swat.patterns.logic.patterns.parameter.Parameter;
 import de.uni.freiburg.iig.telematik.swat.patterns.logic.patterns.parameter.ParameterTypeNames;
 
-public class BorCafterA extends LogCompliancePattern {
+public class CafterAndB extends LogCompliancePattern {
 
-	public BorCafterA() {
+	public CafterAndB() {
 		ArrayList<String> paramTypes = new ArrayList<String>(Arrays.asList(ParameterTypeNames.ACTIVITY));
 		mParameters.add(new Parameter(paramTypes, "A"));
 		mParameters.add(new Parameter(paramTypes, "B"));
@@ -38,6 +38,7 @@ public class BorCafterA extends LogCompliancePattern {
 
 	@Override
 	public PatternRequirements[] requires() {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -52,17 +53,17 @@ public class BorCafterA extends LogCompliancePattern {
 
 	@Override
 	public String getName() {
-		return "B or C after A";
+		return "C afer A and B";
 	}
 
 	@Override
 	public String getDescription() {
-		return "If activity A occured, activity B or C must occur afterwards";
+		return "C must occur after activity A and B";
 	}
 
 	@Override
 	public CompliancePattern duplicate() {
-		BorCafterA duplicate = new BorCafterA();
+		CafterAndB duplicate = new CafterAndB();
 		duplicate.acceptInfoProfider(mInfoProvider);
 		return duplicate();
 	}
@@ -71,48 +72,47 @@ public class BorCafterA extends LogCompliancePattern {
 	public void setFormalization() {
 
 		CompositeRule cr = new CompositeRule();
-
-		//Empty Body
 		Rule r = new Rule(cr);
 		Conjunction body = new Conjunction(r);
 
 		SimpleActivityExecution execA = new SimpleActivityExecution(body, "A", EventType.complete, false);
+		SimpleActivityExecution execB = new SimpleActivityExecution(body, "B", EventType.complete, false);
 		TimeVariable t_a = new TimeVariable(execA);
+		TimeVariable t_b = new TimeVariable(execB);
 
-		if (!paramIsEmpty(0)) {
+		if (!paramIsEmpty(0)) { //A
 			ActivityTypeVariable atv1 = new ActivityTypeVariable(execA);
 			String actName = mParameters.get(0).getValue().getValue();
 			StringConstantAttribute activity1Name = new StringConstantAttribute(actName);
 			new SimpleStringConstraint(atv1, StringOP.EQUAL, activity1Name);
 		}
 
+		if (!paramIsEmpty(1)) { //B
+			ActivityTypeVariable atv2 = new ActivityTypeVariable(execB);
+			String actBName = mParameters.get(1).getValue().getValue();
+			StringConstantAttribute activity2Name = new StringConstantAttribute(actBName);
+			new SimpleStringConstraint(atv2, StringOP.EQUAL, activity2Name);
+		}
+
 		r.setBody(body);
 		Disjunction disjunct = new Disjunction(r);
 		Conjunction conj = new Conjunction(disjunct);
 
-		SimpleActivityExecution execB = new SimpleActivityExecution(conj, "B", EventType.complete, false);
-		TimeVariable timeB = new TimeVariable(execB);
-		RelativeTimeConstraint t_b = new RelativeTimeConstraint(timeB, TimeOP.AFTER, t_a, DisplacementOP.PLUS, new TimeDisplacement());
+		SimpleActivityExecution execC = new SimpleActivityExecution(conj, "C", EventType.complete, false);
 
-		if (!paramIsEmpty(1)) {
-			ActivityTypeVariable atv2 = new ActivityTypeVariable(execB);
-			String actName = mParameters.get(1).getValue().getValue();
-			StringConstantAttribute activity2Name = new StringConstantAttribute(actName);
-			new SimpleStringConstraint(atv2, StringOP.EQUAL, activity2Name);
-		}
-
-		Conjunction conj2 = new Conjunction(disjunct);
-
-		SimpleActivityExecution execC = new SimpleActivityExecution(conj2, "C", EventType.complete, false);
+		//Conjunction conj2 = new Conjunction(r);
 		TimeVariable timeC = new TimeVariable(execC);
-		RelativeTimeConstraint t_c = new RelativeTimeConstraint(timeC, TimeOP.AFTER, t_a, DisplacementOP.PLUS, new TimeDisplacement());
+		RelativeTimeConstraint t_c1 = new RelativeTimeConstraint(timeC, TimeOP.AFTER, t_a, DisplacementOP.PLUS, new TimeDisplacement());
+		RelativeTimeConstraint t_c2 = new RelativeTimeConstraint(timeC, TimeOP.AFTER, t_b, DisplacementOP.PLUS, new TimeDisplacement());
 
 		if (!paramIsEmpty(2)) {
 			ActivityTypeVariable atv3 = new ActivityTypeVariable(execC);
-			String actName = mParameters.get(2).getValue().getValue();
-			StringConstantAttribute activity3Name = new StringConstantAttribute(actName);
+			String actCName = mParameters.get(2).getValue().getValue();
+			StringConstantAttribute activity3Name = new StringConstantAttribute(actCName);
 			new SimpleStringConstraint(atv3, StringOP.EQUAL, activity3Name);
 		}
+
+
 
 		ArrayList<CompositeRule> rules = new ArrayList<CompositeRule>();
 		rules.add(cr);
