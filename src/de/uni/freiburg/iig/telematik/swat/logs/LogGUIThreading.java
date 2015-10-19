248fd245020d111745d98c6351c6a75c807a36a6
jholderer <holderer@iig.uni-freiburg.de>
2015-10-19 17:02:36 +0200

    Added LogGuiThreading

---
+++ src/de/uni/freiburg/iig/telematik/swat/logs/LogGUIThreading.java
@@ -0,0 +1,55 @@
+package de.uni.freiburg.iig.telematik.swat.logs;
+
+import java.util.List;
+import java.util.concurrent.AbstractExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.SwingUtilities;
+
+public class LogGUIThreading extends AbstractExecutorService {
+	private static final LogGUIThreading instance = new LogGUIThreading();
+
+	private LogGUIThreading() {
+	}
+
+	public static LogGUIThreading instance() {
+		return instance;
+	}
+
+	public void execute(Runnable r) {
+		if (SwingUtilities.isEventDispatchThread())
+			r.run();
+		else
+			SwingUtilities.invokeLater(r);
+	}
+
+	@Override
+	public void shutdown() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Runnable> shutdownNow() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isShutdown() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isTerminated() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
