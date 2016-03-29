ef4a1f3fcf960a5745c4d5843845878d56215ad7
tobias <tobias.strickfaden@gmx.de>
2016-03-29 15:10:47 +0200

    fixed bug 39 (export active tab for logs not showing/working) by changing the data type and filechooser dialog and filenames

--- src/de/uni/freiburg/iig/telematik/swat/workbench/action/ExportAction.java
+++ src/de/uni/freiburg/iig/telematik/swat/workbench/action/ExportAction.java
@@ -4,12 +4,11 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.io.File;
 
-
 import javax.swing.JFileChooser;
 import javax.swing.KeyStroke;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
 import de.invation.code.toval.file.FileUtils;
+import de.uni.freiburg.iig.telematik.swat.logs.LogFileViewer;
 import de.uni.freiburg.iig.telematik.swat.logs.LogModel;
 import de.uni.freiburg.iig.telematik.swat.workbench.components.SwatComponents;
 import de.uni.freiburg.iig.telematik.swat.workbench.SwatTabView;
@@ -36,13 +35,9 @@ public class ExportAction extends AbstractWorkbenchAction {
 		dialog.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		
 		if (current instanceof PNEditorComponent) {
-            //file = SwatComponents.getInstance().getContainerPetriNets().getComponent(((PNEditorComponent) current).getNetContainer().getPetriNet().getName());
 			String name = (((PNEditorComponent) current).getNetContainer().getPetriNet().getName()+".pnml");
             file = new File (SwatComponents.getInstance().getContainerPetriNets().getBasePath() + ((PNEditorComponent) current).getNetContainer().getPetriNet().getName() + "/", name);
-            //file = SwatComponents.getInstance().getPetriNetFile(((PNEditorComponent) current).getNetContainer().getPetriNet().getName());
-            
-            int returnValue = dialog.showSaveDialog(Workbench.getInstance());
-			if (returnValue == JFileChooser.APPROVE_OPTION) {
+			if (dialog.showSaveDialog(Workbench.getInstance()) == JFileChooser.APPROVE_OPTION) {
 				try {
 					String filename = dialog.getSelectedFile().toString() + "/" + name;
 					File save = new File(filename);
@@ -52,25 +47,22 @@ public class ExportAction extends AbstractWorkbenchAction {
 					e1.printStackTrace();
 				}
 			}
-		} else if (current instanceof LogModel) {
-			file = ((LogModel) current).getFileReference();
-            int returnValue = dialog.showSaveDialog(Workbench.getInstance());
-			if (returnValue == JFileChooser.APPROVE_OPTION) {
+			
+		} else if (current instanceof LogFileViewer) {
+			file = ((LogFileViewer) current).getFileReference();
+			System.out.println(file.toString());
+			if (dialog.showSaveDialog(Workbench.getInstance()) == JFileChooser.APPROVE_OPTION) {
 				try {
-					FileUtils.copy(file, dialog.getSelectedFile());
+					String path = dialog.getSelectedFile().toString() + "/";
+					String filename = file.toString();
+					path += filename.substring(filename.lastIndexOf("/")+1);
+					File save = new File(path);
+					FileUtils.copy(file, save);
 				} catch (Exception e1) {
 					Workbench.errorMessage("Could not copy " + file + " to " + dialog.getSelectedFile(), e1, true);
 					e1.printStackTrace();
 				}
 			}
 		}
-
-
-			
-			
-			
-
-
 	}
-
 }
