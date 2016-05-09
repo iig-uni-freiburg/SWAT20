fcdf4cd4a37be9b161a7aa06bfd19098c9a693f0
rzahoransky <r.zahoransky@gmx.de>
2016-05-09 14:27:49 +0200

    Workflow Execution Plan added

--- src/de/uni/freiburg/iig/telematik/swat/simulation/PlanExtractor.java
+++ src/de/uni/freiburg/iig/telematik/swat/simulation/PlanExtractor.java
@@ -8,6 +8,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 import de.invation.code.toval.misc.wd.ProjectComponentException;
 import de.invation.code.toval.parser.ParserException;
@@ -65,7 +67,11 @@ public class PlanExtractor {
 		
 		System.out.println("number of plans: "+singlePlans.size());
 		
-		generatePlans();
+		TreeMap<Double, FireSequence> plan = generatePlans();
+		
+		for(Entry<Double, FireSequence> entry:plan.entrySet()){
+			System.out.println(entry.getKey()+": "+entry.getValue().getTransitionString());
+		}
 		
 
 	}
@@ -80,24 +86,41 @@ public class PlanExtractor {
 		
 	}
 	
-	private void generatePlans(){
-		HashSet<String> differentPlanSet = new HashSet<>();
+	private TreeMap<Double, FireSequence> generatePlans(){
+		TreeMap<WorkflowExecutionPlan,WorkflowExecutionPlan> differentPlanSet = new TreeMap<>();
 		HashMap<FireSequence, LinkedList<Double>> performance = new HashMap<>();
 		for(FireSequence seq: listener.getOverallLog()){
-			differentPlanSet.add(seq.getTransitionString());
+			WorkflowExecutionPlan plan = new WorkflowExecutionPlan(seq);
+			differentPlanSet.put(plan,plan);
 			if(!performance.containsKey(seq)){ //create list
 				performance.put(seq,new LinkedList<Double>());
 			}
-			
-			//add OverallPerformance
-			performance.get(seq).add(getOverallSuccessRatio(seq));
 		}
+		
+		for(FireSequence seq:listener.getOverallLog()) //add OverallPerformance
+		performance.get(seq).add(getOverallSuccessRatio(seq));
+		
+		for(FireSequence seq:listener.getOverallLog()){
+			WorkflowExecutionPlan plan = differentPlanSet.get(seq);
+			if(plan!=null)
+				plan.addSequence(seq);
+		}
+		
+		for(WorkflowExecutionPlan seq:differentPlanSet.keySet()){
+			System.out.println(seq);
+		}
+		
+		TreeMap<Double, FireSequence> result = new TreeMap<>();
 		
 		for(Entry<FireSequence, LinkedList<Double>> entry:performance.entrySet()){
-			System.out.println(entry.getKey().getTransitionString()+": "+computeAverage(entry.getValue())); //sort this
+			System.out.println(entry.getValue().size()+" entries for: "+entry.getKey().getTransitionString());
+			result.put(computeAverage(entry.getValue()),entry.getKey());
 		}
 		
-		System.out.println("Number of different runnings: "+differentPlanSet.size());
+		System.out.println("Number of different runnings: "+differentPlanSet.size()); //size is wrong
+		
+		return result;
+		
 	}
 	
 	private double getOverallSuccessRatio(FireSequence seq){
